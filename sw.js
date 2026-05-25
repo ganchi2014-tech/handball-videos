f99a3bed7dc5cb49475f61eb4adc6863e408a24e
ganchi2014-tech <ganchi2014-tech@users.noreply.github.com>
2026-05-25 14:30:19 +0900

    Add separate buttons: video (original) and translation (proxy)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nav-v2';
+const CACHE = 'nav-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
