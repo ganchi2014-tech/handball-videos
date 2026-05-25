928075d0a55cdf9ca6182ef9229d7e8ad985f6ea
ganchi2014-tech <ganchi2014-tech@users.noreply.github.com>
2026-05-25 14:21:47 +0900

    Wrap video links with Google Translate proxy (no -> ja)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nav-v1';
+const CACHE = 'nav-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
