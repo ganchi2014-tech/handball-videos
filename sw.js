6d7660b35d2281df595cc1b98129275e464fc261
ganchi2014-tech <ganchi2014-tech@users.noreply.github.com>
2026-05-25 14:36:34 +0900

    Revert to original URLs; add browser translation guide tip

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nav-v3';
+const CACHE = 'nav-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
